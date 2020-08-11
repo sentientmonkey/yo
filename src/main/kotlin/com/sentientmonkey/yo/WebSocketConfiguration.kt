17558c54338c85a68a2db3d4c16fa03e33bdf94e
Scott Windsor <swindsor@gmail.com>
2020-08-11 10:52:47 -0700

    Enable true/false flag

--- src/main/kotlin/com/sentientmonkey/yo/WebSocketConfiguration.kt
+++ src/main/kotlin/com/sentientmonkey/yo/WebSocketConfiguration.kt
@@ -7,25 +7,31 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
-
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfiguration(
+        @Value("\${websocket.relay.enabled:true}") val relayEnabledString: String,
         @Value("\${websocket.relay.host:localhost}") val relayHost: String,
         @Value("\${websocket.relay.port:61613}") val relayPort: Int,
         @Value("\${websocket.client.login:guest}") val clientLogin: String,
         @Value("\${websocket.client.passcode:guest}") val clientPasscode: String
 ) : WebSocketMessageBrokerConfigurer {
+
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/websocket-app").withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.setApplicationDestinationPrefixes("/app")
-                .enableStompBrokerRelay("/topic")
-                .setRelayHost(relayHost)
-                .setRelayPort(relayPort)
-                .setClientLogin(clientLogin)
-                .setClientPasscode(clientPasscode)
+
+        val enableRelay = relayEnabledString.trim().toBoolean()
+
+        if (enableRelay) {
+            registry.enableStompBrokerRelay("/topic")
+                    .setRelayHost(relayHost)
+                    .setRelayPort(relayPort)
+                    .setClientLogin(clientLogin)
+                    .setClientPasscode(clientPasscode)
+        }
     }
 }
